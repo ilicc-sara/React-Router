ebc9fb89ad24f833fe55c72d33d984665413834a
Sara <sarailic431@gmail.com>
2025-07-20 18:33:08 +0200

    .

--- src/ProfilesPage.jsx
+++ src/ProfilesPage.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, NavLink } from "react-router-dom";
 
 export default function ProfilesPage() {
   const profiles = [1, 2, 3, 4, 5];
@@ -6,10 +6,10 @@ export default function ProfilesPage() {
   return (
     <div className="profiles-cont">
       {profiles.map((profile) => (
-        <Link key={profile} to={`/profiles/${profile}`}>
+        <NavLink key={profile} to={`/profiles/${profile}`}>
           {" "}
           Profile {profile}{" "}
-        </Link>
+        </NavLink>
       ))}
       <Outlet />
     </div>
