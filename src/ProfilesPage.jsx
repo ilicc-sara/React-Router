78714f0cb0c711348fca4e56099c830226c371ed
Sara <sarailic431@gmail.com>
2025-07-20 17:59:36 +0200

    .

--- src/ProfilesPage.jsx
+++ src/ProfilesPage.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function ProfilesPage() {
   const profiles = [1, 2, 3, 4, 5];
 
   return (
-    <div>
-      <h1>Profiles Page</h1>
+    <div className="profiles-cont">
+      {profiles.map((profile) => (
+        <Link key={profile} to={`/profiles/${profile}`}>
+          {" "}
+          Profile {profile}{" "}
+        </Link>
+      ))}
     </div>
   );
 }
