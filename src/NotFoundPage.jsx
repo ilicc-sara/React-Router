49a9dd604865e6a76c4c274d70742a4e8a6ecccc
Sara <sarailic431@gmail.com>
2025-07-20 17:49:24 +0200

    error page and return to home

---
+++ src/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <h1>404 Not Found</h1>
+
+      <Link to="">Home</Link>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./HomePage.jsx";
+import ProfilesPage from "./ProfilesPage.jsx";
+import NotFoundPage from "./NotFoundPage.jsx";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // component: HomePage,
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/profiles",
+    element: <ProfilesPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
